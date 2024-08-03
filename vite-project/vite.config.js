5c5b1b8e368a08da6748ad4df4f1545c17f1a6ac
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-08-03 23:51:31 +0900

    pug

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,111 +1,47 @@
 import {
   defineConfig
-} from 'vite';
-import {
-  resolve
-} from 'path';
-import fs from 'fs';
-import path from 'path';
-
-const files = [];
-
-function readDirectory(dirPath) {
-  const items = fs.readdirSync(dirPath);
-
-  for (const item of items) {
-    const itemPath = path.join(dirPath, item);
-
-    if (fs.statSync(itemPath).isDirectory()) {
-      // componentsディレクトリを除外する
-      if (item === 'components') {
-        continue;
-      }
-
-      readDirectory(itemPath);
-    } else {
-      // htmlファイル以外を除外する
-      if (path.extname(itemPath) !== '.html') {
-        continue;
-      }
-
-      // nameを決定する
-      let name;
-      if (dirPath === path.resolve(__dirname, 'src')) {
-        name = path.parse(itemPath).name;
-      } else {
-        const relativePath = path.relative(path.resolve(__dirname, 'src'), dirPath);
-        const dirName = relativePath.replace(/\//g, '_');
-        name = `${dirName}_${path.parse(itemPath).name}`;
-      }
-
-      // pathを決定する
-      const relativePath = path.relative(path.resolve(__dirname, 'src'), itemPath);
-      const filePath = `/${relativePath}`;
-
-      files.push({
-        name,
-        path: filePath
-      });
-    }
-  }
-}
-readDirectory(path.resolve(__dirname, 'src'));
-const inputFiles = {};
-for (let i = 0; i < files.length; i++) {
-  const file = files[i];
-  inputFiles[file.name] = resolve(__dirname, './src' + file.path);
-}
-
-import handlebars from 'vite-plugin-handlebars';
-
-//HTML上で出し分けたい各ページごとの情報
-const pageData = {
-  '/index.html': {
-    isHome: true,
-    title: 'Shiori Inoue',
-  },
-  // '/list.html': {
-  //   isHome: false,
-  //   title: 'List Page',
-  // },
-};
+} from "vite";
+import vitePluginPugStatic from "@macropygia/vite-plugin-pug-static";
+import eslint from '@nabla/vite-plugin-eslint';
 
 export default defineConfig({
-  root: './src', //開発ディレクトリ設定
+  root: "src",
   build: {
-    outDir: '../dist', //出力場所の指定
+    outDir: "../dist",
     emptyOutDir: true,
-    rollupOptions: { //ファイル出力設定
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.')[1];
-          //Webフォントファイルの振り分け
-          if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
-            extType = 'fonts';
-          }
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'images';
-          }
-          //ビルド時のCSS名を明記してコントロールする
-          if (extType === 'css') {
-            return `assets/css/style.css`;
-          }
-          return `assets/${extType}/[name][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name].js',
-        entryFileNames: 'assets/js/[name].js',
+    rollupOptions: {
+      input: {
+        main: "src/index.pug",
+        // about: "src/about/about.pug"
       },
-      input: inputFiles,
-    },
+      output: {
+        entryFileNames: `assets/js/[name].js`,
+        chunkFileNames: `assets/js/[name].js`,
+        assetFileNames: (assetInfo) => {
+          if (/\.( gif|jpeg|jpg|png|svg|webp| )$/.test(assetInfo.name)) {
+            return 'assets/images/[name].[ext]';
+          }
+          if (/\.css$/.test(assetInfo.name)) {
+            return 'assets/css/[name].[ext]';
+          }
+          return 'assets/[name].[ext]';
+        }
+      }
+    }
   },
   plugins: [
-    handlebars({
-      //コンポーネントの格納ディレクトリを指定
-      partialDirectory: resolve(__dirname, './src/components'),
-      //各ページ情報の読み込み
-      context(pagePath) {
-        return pageData[pagePath];
-      },
+    vitePluginPugStatic({
+      buildOptions: {
+        basedir: "src"
+      }, //ルートディレクトリ
+      serveOptions: {
+        basedir: "src"
+      } //ルートディレクトリ
     }),
-  ],
-});
+    eslint({
+      eslintOptions: {
+        fix: true,
+      }
+    }),
+  ]
+})
