3a39e9ffb7e84991a25f55604908bb286807a05d
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-10-27 21:46:53 +0900

    編集

--- vite-project/src/assets/js/main.js
+++ vite-project/src/assets/js/main.js
@@ -1,5 +1,4 @@
 `use strict`;
-console.log('hello');
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -8,29 +7,33 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  let worksList = document.querySelector('#year');
-  let defaultValue = document.querySelector('#defaultValue');
-  let defaultAnchor = defaultValue.querySelector('a');
-  let worksYearItems = document.querySelectorAll('.worksYear_item');
+  const worksYearList = document.querySelector('.worksYear_list');
+  if (worksYearList && worksYearList.classList.contains('worksYear_list')) {
+    console.log('true');
+    let worksList = document.querySelector('#year');
+    let defaultValue = document.querySelector('#defaultValue');
+    let defaultAnchor = defaultValue.querySelector('a');
+    let worksYearItems = document.querySelectorAll('.worksYear_item');
 
-  if (!defaultAnchor.textContent.trim()) {
-    defaultAnchor.textContent = worksYearItems[1].querySelector('a').textContent;
-  }
-
-  worksList.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    if (worksList.classList.contains("worksYear_listVisible")) {
-      worksList.classList.remove('worksYear_listVisible');
-    } else {
-      worksList.classList.add('worksYear_listVisible');
+    if (!defaultAnchor.textContent.trim()) {
+      defaultAnchor.textContent = worksYearItems[1].querySelector('a').textContent;
     }
-  });
 
-  worksYearItems.forEach(item => {
-    item.addEventListener('click', (event) => {
-      let selectedText = item.querySelector('a').textContent;
-      defaultAnchor.textContent = selectedText;
+    worksList.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      if (worksList.classList.contains("worksYear_listVisible")) {
+        worksList.classList.remove('worksYear_listVisible');
+      } else {
+        worksList.classList.add('worksYear_listVisible');
+      }
     });
-  });
+
+    worksYearItems.forEach(item => {
+      item.addEventListener('click', (event) => {
+        let selectedText = item.querySelector('a').textContent;
+        defaultAnchor.textContent = selectedText;
+      });
+    });
+  }
 });
