f0a228160a60b13635cf30dd56def1ad0f18983e
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2024-12-05 21:47:02 +0900

    追加

--- portfolio-wp/shiori-portfolio/index.php
+++ portfolio-wp/shiori-portfolio/index.php
@@ -4,8 +4,15 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  
+<?php
+// シンプルなテーマテンプレート
+get_header();
+?>
+<h1>Welcome to My Theme</h1>
+<?php
+get_footer();?>
 </body>
 </html>
