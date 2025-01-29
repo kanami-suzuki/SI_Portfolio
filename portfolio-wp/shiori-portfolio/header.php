76a091901c39e383b131e5d720e829babdb0f8c7
kanami_suzuki <58844108+kanami-suzuki@users.noreply.github.com>
2025-01-29 20:12:40 +0900

    title

--- portfolio-wp/shiori-portfolio/header.php
+++ portfolio-wp/shiori-portfolio/header.php
@@ -6,9 +6,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
-    <meta name="viewport" content="width=device-width">
     <meta name="description" content="<?php bloginfo('description'); ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,4 +17,12 @@
     //StylesheetやWebfontはfunctions
     wp_head(); ?>
 </head>
+<body>
+<?php wp_body_open(); ?>
 
+<div>
+
+</div>
+<header>
+
+</header>
